04597dab298f82c956fc9c81849b4db633058eb1
nazhang8888 <nazhang@pm.me>
2023-10-14 17:57:20 -0400

    fix: parameterized query

--- packages/api/scripts/seed-db.ts
+++ packages/api/scripts/seed-db.ts
@@ -5,5 +5,8 @@ await db?.execute(
 	"CREATE TABLE IF NOT EXISTS tracked_skins (id text primary key, steamid text, exterior integer, category integer, item text, name text);",
 );
 await db?.execute(
-	`INSERT INTO tracked_skins VALUES (${randomUUID()}, '76561198065741354', 0, 1, 'Bayonet', 'Doppler');`,
+	{
+		sql: `INSERT INTO tracked_skins VALUES (?, ?, ?, ?, ?, ?);`,
+		args: [randomUUID(), "76561198065741354", 0, 1, "Bayonet", "Doppler"],
+	}	
 );
