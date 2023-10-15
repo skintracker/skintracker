40fbb4cea214a6c5f1583d50fedfcd597751f700
Jackson Salopek <jackson.salopek@gmail.com>
2023-10-15 00:24:35 -0400

    Co-authored-by: Charles Rooney <crooney36@users.noreply.github.com> Co-authored-by: nazhang8888 <nazhang8888@users.noreply.github.com>

--- packages/api/src/routes/index.tsx
+++ packages/api/src/routes/index.tsx
@@ -1,26 +1,22 @@
 import { captureException } from "@/utils/sentry";
 import { AponiaCtx, AponiaRouteHandler, AponiaRouteHandlerFn } from "aponia";
 import { getHealthcheck } from "./api/healthcheck";
+import { BaseLayout } from "@/layouts/base";
+import { Button } from "@/components/button";
 
 export const getIndex: AponiaRouteHandlerFn<JSX.Element> = (
   _ctx: AponiaCtx
 ) => {
   const healthcheckRes = getHealthcheck(_ctx);
   return (
-    <html lang="en">
-      <head>
-        <title>Skintracker</title>
-        <script src="/public/js/htmx.js" />
-        <script src="/public/js/tailwind.js" />
-      </head>
-      <body>
-        <div>
-          <h1>Hello World</h1>
-          <img alt="POG" src="/public/test-img.png" />
-          <p>Healthcheck: {JSON.stringify(healthcheckRes)}</p>
-        </div>
-      </body>
-    </html>
+    <BaseLayout>
+      <div>
+        <h1>Hello World</h1>
+        <img alt="POG" src="/public/test-img.png" />
+        <p>Healthcheck: {JSON.stringify(healthcheckRes)}</p>
+        <Button text="Login" onClickRoute="/client/home/buttons" />
+      </div>
+    </BaseLayout>
   );
 };
 
