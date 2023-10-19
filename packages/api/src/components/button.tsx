cf39512ab75e0ff3e519ed85e295ff6194548af2
Jackson Salopek <jackson.salopek@gmail.com>
2023-10-19 01:47:41 -0400

    fix: navbar buttons

--- packages/api/src/components/button.tsx
+++ packages/api/src/components/button.tsx
@@ -1,37 +1,44 @@
 export enum ButtonType {
-  Default = 0,
-  Ghost = 1,
+	Default = 0,
+	Ghost = 1,
 }
 
 export interface ButtonProps {
-  text: string;
-  classes?: string;
-  type?: ButtonType;
-  href?: string;
+	text?: string;
+	children?: JSX.Element | JSX.Element[];
+	classes?: string;
+	type?: ButtonType;
+	href?: string;
 }
 
-export function Button({ classes, type, text, href, ...props }: ButtonProps) {
-  const classStr = (() => {
-    switch (type) {
-      case ButtonType.Ghost:
-        return `p-2${classes ? ` ${classes}` : ""}`;
-      default:
-        return `border border-solid border-slate-200 rounded p-2${
-          classes ? ` ${classes}` : ""
-        }`;
-    }
-  })();
-  return (
-    <button
-      type="button"
-      class={classStr}
-      {...(href ? { onclick: `location.href='${href}'` } : {})}
-      {...props}
-      safe
-    >
-      {text}
-    </button>
-  );
+export function Button({
+	children,
+	classes,
+	type,
+	text,
+	href,
+	...props
+}: ButtonProps) {
+	const classStr = (() => {
+		switch (type) {
+			case ButtonType.Ghost:
+				return `flex p-2${classes ? ` ${classes}` : ""}`;
+			default:
+				return `flex border border-solid border-slate-200 rounded p-2${
+					classes ? ` ${classes}` : ""
+				}`;
+		}
+	})();
+	return (
+		<button
+			type="button"
+			class={classStr}
+			{...(href ? { onclick: `location.href='${href}'` } : {})}
+			{...props}
+		>
+			{children ?? text ?? ""}
+		</button>
+	);
 }
 
 export default Button;
