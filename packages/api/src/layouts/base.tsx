b5665aae546fa1960160d70e66f8f8ae3d5d0476
Jackson Salopek <jackson.salopek@gmail.com>
2024-02-08 05:21:24 -0500

    V2024.1.1 (#49)
    * fix: access host
    
    * fix: update deps
    
    * fix: re-add defer
    
    * feat: initial logout experience
    
    * fix: working redirects
    
    * feat: encode everything with gzip
    
    * feat: add fork to enable file compression
    
    * Fix GZIP (#48)
    
    * fix: prod build
    
    * fix: revert

--- packages/api/src/layouts/base.tsx
+++ packages/api/src/layouts/base.tsx
@@ -19,7 +19,7 @@ export function BaseLayout({ children, title, user }: BaseLayoutProps) {
     if (Bun.env.NODE_ENV === "production")
       return (
         <>
-          <link rel="stylesheet" href="/public/css/styles.e2ceca52.css" />
+          <link rel="stylesheet" href="/public/css/styles.21f49359.css" />
           <link rel="stylesheet" href="/public/css/global.css" />
         </>
       );
