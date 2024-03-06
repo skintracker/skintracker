4a0743efb40d24de99832e52b8e2f5e16b1fa31d
Jackson Salopek <jackson.salopek@gmail.com>
2024-03-06 15:21:23 +0100

    improve: add nix for shell management

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Skintracker";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
