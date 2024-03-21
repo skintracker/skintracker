363160ed709eaf75170093610b778362db0c0096
Jackson Salopek <jackson.salopek@gmail.com>
2024-03-21 01:28:48 -0400

    feat: bump nixpkgs to master

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Skintracker";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
